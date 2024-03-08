f332b9f78d374f34c466dd0537e67d467867a44c
Vu Thuy Van Uyen <vuthuyvanuyen@Vus-MacBook-Pro.local>
2024-03-08 20:15:21 +0700

    add new theme

---
+++ wp-admin/maint/repair.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="robots" content="noindex,nofollow">
+	<title>WordPress &rsaquo; Database Repair</title>
+	<link rel="stylesheet" id="all-css-b292ed5cd5890cc31e0125e2fa693143" href="/_static/?-eJwrL9DNzEvOKU1JLdZPLi7WTyotKcnPK9bLzczTA/J1ygt0E1OAHLBkWn5RLg6pHEODPOwymXnFJYk5OTBJ_1xbQzNLc1MTCwsjYwCicSzt" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=6.4.3" media="all">
+</head>
+<body class="wp-core-ui">
+<p id="logo"><a href="https://wordpress.org/">WordPress</a></p>
+
+<h1 class="screen-reader-text">Allow automatic database repair</h1><p>To allow use of this page to automatically repair database problems, please add the following line to your <code>wp-config.php</code> file. Once this line is added to your config, reload this page.</p><p><code>define('WP_ALLOW_REPAIR', true);</code></p></body>
+</html>
